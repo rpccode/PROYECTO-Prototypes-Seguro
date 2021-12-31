eca40552261e2af0837dfd52d064755767afc0cf
Rudy Casilla <rpccode@gmail.com>
2021-12-31 18:27:19 -0500

    Comenzar el calculo del seguro

--- js/app.js
+++ js/app.js
@@ -5,6 +5,53 @@ function  Seguro(marca,year,tipo) {
     this.tipo = tipo;
 }
 
+//realiza la cotizacion con los datos
+
+Seguro.prototype.cotizarSeguro = function(){
+    /*
+            1= 1.15
+            2=1.05
+            3=1.35
+    */
+    let cantidad ;
+    let base = 2000;
+
+   switch (this.marca) {
+       case '1':
+                cantidad = base * 1.15;
+                break;
+       case '2':
+                cantidad = base * 1.05;
+                break;
+       case '3':
+                cantidad = base * 1.35;
+           break;
+   
+       default:
+           break;
+   }
+   //leer el ano
+    const diferencia = new Date().getFullYear() - this.year;
+   //cada año que la diferencia es myor el costo va a reducir un 3%
+   cantidad -= ((diferencia * 3) * cantidad)/100;
+
+   /**
+    * 
+    *  si el Seguro es basico se multiplica por un  30% mas
+    * si el Seguro es completo se multiplica por un  50% mas
+    * 
+    * */
+   if (this.tipo === 'basico') {
+            cantidad *= 1.30;
+   }else{
+            cantidad *= 1.50;
+   }
+
+
+   console.log(cantidad);
+    return cantidad;
+}
+
 function UI() {}
 
 //llena las opciones de los años
@@ -96,8 +143,8 @@ function cotizarSeguro(e){
                     ui.mostrarMensaje('pasaste la validación', 'correcto');
 
             //instaciando Seguro
-
-
+                const seguro = new Seguro(marca, year, tipo);
+                seguro.cotizarSeguro(); 
 
             //utilizar el prototype que va a seleccionadar
             
